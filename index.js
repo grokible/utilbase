d80b8542d6c499a8e889738a0a151ecc02578623
Roger Bush <roger@grokible.com>
2017-10-22 01:05:37 +0000

    Not sure

---
+++ index.js
@@ -0,0 +1,13 @@
+
+var UtilBase = {};
+
+UtilBase.Exception = require ('./exception');
+UtilBase.HttpException = require ('./httpException');
+UtilBase.Oop = require ('./oop');
+UtilBase.Opt = require ('./opt');
+UtilBase.PromiseUtil = require ('./promiseUtil');
+UtilBase.Prop = require ('./prop');
+UtilBase.RestCaller = require ('./restCaller');
+UtilBase.RestUtil = require ('./restUtil');
+
+exports = UtilBase;
